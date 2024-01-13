0b039f0d6249787ed647711ab8f195c25e27b3ee
Aniket-2412 <154119007+Aniket-2412@users.noreply.github.com>
2024-01-13 18:59:08 +0530

    Add files via upload

---
+++ instagram_like/instagram_like.js
@@ -0,0 +1,10 @@
+var a=document.querySelector(".container");
+var icon=document.querySelector("i");
+
+
+a.addEventListener("dblclick",function(){
+    icon.style.transform="translate(-50%,-50%) scale(1)";
+    setTimeout(function(){
+        icon.style.transform="translate(-50%,-50%) scale(0)";
+    }, 1000);
+})
